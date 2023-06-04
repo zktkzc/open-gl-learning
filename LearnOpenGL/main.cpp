1738efa88904448c5b32a96ff5a6460b3653c9a0
tkzc00 <2238983030@qq.com>
2023-06-04 15:11:42 +0800

    重构代码

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -10,6 +10,7 @@
 #include <glm/gtc/type_ptr.hpp>
 #include "Camera.h"
 
+#pragma region 模型数据
 // 顶点数组
 float vertices[] = {
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -55,19 +56,6 @@ float vertices[] = {
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-float lastX; // 鼠标上一帧X位置
-float lastY; // 鼠标上一帧Y位置
-bool firstMouse = false; // 是否是第一次鼠标进入窗口
-
-// Camera camera(glm::vec3(0, 0, 3.0f), glm::vec3(0, 0, 0), glm::vec3(0, 1.0f, 0)); // 创建一个摄像机
-Camera camera(glm::vec3(0, 0, 3.0f), 15.0f, 180.0f, glm::vec3(0, 1.0f, 0)); // 创建一个摄像机
-
-// 索引数组
-unsigned int indices[] = { // 逆时针绘制为正面
-	0, 1, 2,
-	2, 3, 0
-};
-
 // 多个立方体位置向量
 glm::vec3 cubePositions[] = {
   glm::vec3(0.0f,  0.0f,  0.0f),
@@ -81,6 +69,17 @@ glm::vec3 cubePositions[] = {
   glm::vec3(1.5f,  0.2f, -1.5f),
   glm::vec3(-1.3f,  1.0f, -1.5f)
 };
+#pragma endregion
+
+#pragma region 创建摄像机
+Camera camera(glm::vec3(0, 0, 3.0f), 15.0f, 180.0f, glm::vec3(0, 1.0f, 0)); // 创建一个摄像机
+#pragma endregion
+
+#pragma region 处理键盘和鼠标输入
+float lastX; // 鼠标上一帧X位置
+float lastY; // 鼠标上一帧Y位置
+bool firstMouse = false; // 是否是第一次鼠标进入窗口
+
 
 void processInput(GLFWwindow* window)
 {
@@ -116,9 +115,43 @@ void mouse_callback(GLFWwindow* window, double xPos, double yPos)
 
 	camera.ProcessMouseMovement(deltaX, deltaY); // 处理鼠标移动
 }
+#pragma endregion
+
+unsigned int LoadImageToGPU(const char* filename, GLint internalFormat, GLenum format, int textureSlot)
+{
+	unsigned int TexBuffer; // 纹理缓冲区对象
+	glGenTextures(1, &TexBuffer); // 生成纹理
+	glActiveTexture(GL_TEXTURE0 + textureSlot); // 激活纹理单元
+	glBindTexture(GL_TEXTURE_2D, TexBuffer); // 绑定纹理
+	// 设置纹理环绕方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // 设置S轴环绕方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // 设置T轴环绕方式
+	// 设置纹理过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // 设置缩小过滤方式
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // 设置放大过滤方式
+
+
+	// 加载并生成纹理
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); // 翻转图片
+	unsigned char* data = stbi_load(filename, &width, &height, &nrChannels, 0); // 加载图片
+	if (data)
+	{
+		// 生成纹理
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data); // 生成纹理
+		glGenerateMipmap(GL_TEXTURE_2D); // 为当前绑定的纹理自动生成所有需要的多级渐远纹理
+	}
+	else
+	{
+		printf("Failed to load texture!");
+	}
+	stbi_image_free(data); // 释放图像的内存
+	return TexBuffer;
+}
 
 int main()
 {
+#pragma region 初始化GLFW，配置GLFW，创建窗口
 	// 初始化GLFW
 	glfwInit();
 	// 配置GLFW，使用的版本为3.3，核心模式
@@ -151,16 +184,14 @@ int main()
 	// 设置视窗位置、大小
 	glViewport(0, 0, 800, 600);
 
-	// 背面剔除
-	// glEnable(GL_CULL_FACE); // 启用面剔除
-	// glCullFace(GL_BACK); // 剔除背面
-
-	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // 设置绘制模式为线框模式
-
 	glEnable(GL_DEPTH_TEST); // 启用深度测试
+#pragma endregion
 
-	Shader* myShader = new Shader("vertexShaderSource.txt", "fragmentShaderSource.txt");
+#pragma region 创建着色器程序
+	Shader* myShader = new Shader("vertexShaderSource.vert", "fragmentShaderSource.frag");
+#pragma endregion
 
+#pragma region 初始化顶点数据，绑定VAO和VBO
 	// 生成一个顶点数组对象(Vertex Array Object, VAO)
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
@@ -175,121 +206,60 @@ int main()
 	// 把用户定义的数据复制到当前绑定缓冲的函数
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	unsigned int EBO; // 索引缓冲对象(Element Buffer Object, EBO)
-	glGenBuffers(1, &EBO); // 生成一个索引缓冲对象
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // 把新创建的缓冲绑定到GL_ELEMENT_ARRAY_BUFFER目标上
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // 把用户定义的数据复制到当前绑定缓冲的函数
-
 	// 设置顶点属性指针
 	glVertexAttribPointer(6, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)0); // 解析顶点数据
 	glEnableVertexAttribArray(6); // 启用顶点属性
-	// glVertexAttribPointer(7, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (GLvoid*)(3 * sizeof(float))); // 解析颜色数据
-	// glEnableVertexAttribArray(7); // 启用顶点属性
 	glVertexAttribPointer(8, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (GLvoid*)(3 * sizeof(float))); // 解析纹理坐标数据
 	glEnableVertexAttribArray(8); // 启用顶点属性
+#pragma endregion
 
+#pragma region 加载纹理
 	// 纹理
 	unsigned int TexBufferA;
-	glGenTextures(1, &TexBufferA); // 生成纹理
-	glActiveTexture(GL_TEXTURE0); // 激活纹理单元
-	glBindTexture(GL_TEXTURE_2D, TexBufferA); // 绑定纹理
-	// 设置纹理环绕方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // 设置S轴环绕方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // 设置T轴环绕方式
-	// 设置纹理过滤方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // 设置缩小过滤方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // 设置放大过滤方式
-
-	// 加载并生成纹理
-	int width, height, nrChannels;
-	stbi_set_flip_vertically_on_load(true); // 翻转图片
-	unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0); // 加载图片
-	if (data)
-	{
-		// 生成纹理
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data); // 生成纹理
-		glGenerateMipmap(GL_TEXTURE_2D); // 为当前绑定的纹理自动生成所有需要的多级渐远纹理
-	}
-	else
-	{
-		printf("Failed to load texture!");
-	}
-	stbi_image_free(data); // 释放图像的内存
+	TexBufferA = LoadImageToGPU("container.jpg", GL_RGB, GL_RGB, 0);
 
 	unsigned int TexBufferB;
-	glGenTextures(1, &TexBufferB); // 生成纹理
-	glActiveTexture(GL_TEXTURE3); // 激活纹理单元
-	glBindTexture(GL_TEXTURE_2D, TexBufferB); // 绑定纹理
+	TexBufferB = LoadImageToGPU("awesomeface.png", GL_RGBA, GL_RGBA, 3);
+#pragma endregion
 
-	// 设置纹理环绕方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT); // 设置S轴环绕方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT); // 设置T轴环绕方式
-	// 设置纹理过滤方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // 设置缩小过滤方式
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // 设置放大过滤方式
-
-	// 加载并生成纹理
-	data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0); // 加载图片
-	if (data)
-	{
-		// 生成纹理
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // 生成纹理
-		glGenerateMipmap(GL_TEXTURE_2D); // 为当前绑定的纹理自动生成所有需要的多级渐远纹理
-	}
-	else
-	{
-		printf("Failed to load texture!");
-	}
-	stbi_image_free(data); // 释放图像的内存
-
-	glm::mat4 trans; // 创建一个四维数组
-	// trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0)); // 位移
-	// trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1.0f)); // 旋转
-	// trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f)); // 缩放
-
+#pragma region 准备模型矩阵，观察矩阵，投影矩阵
 	glm::mat4 modelMat; // 模型矩阵
-	modelMat = glm::rotate(modelMat, glm::radians(-55.0f), glm::vec3(1.0f, 0, 0)); // 旋转
-
 	glm::mat4 viewMat; // 观察矩阵
-	// viewMat = glm::translate(viewMat, glm::vec3(0.0f, 0.0f, -3.0f)); // 位移
-	// viewMat = camera.GetViewMatrix(); // 获取观察矩阵
-
 	glm::mat4 projectionMat; // 投影矩阵
 	projectionMat = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f); // 透视投影
+#pragma endregion
 
+#pragma region 渲染循环
 	while (!glfwWindowShouldClose(window)) // 每次循环的开始前检查一次GLFW是否被要求退出
 	{
-		processInput(window);
+		processInput(window); // 处理输入
 
 		// 清除颜色缓冲之后，整个颜色缓冲都会被填充为glClearColor里所设置的颜色
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		// 清除颜色缓冲
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // 同时清除深度缓冲
 
-		glActiveTexture(GL_TEXTURE0); // 激活纹理单元
-		glBindTexture(GL_TEXTURE_2D, TexBufferA); // 绑定纹理
-		glActiveTexture(GL_TEXTURE3); // 激活纹理单元
-		glBindTexture(GL_TEXTURE_2D, TexBufferB); // 绑定纹理
-		glBindVertexArray(VAO); // 绑定VAO
-		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // 绑定EBO
-
 		viewMat = camera.GetViewMatrix(); // 获取观察矩阵
 
 		for (int i = 0; i < 10; i++)
 		{
-			glm::mat4 modelMat2; // 模型矩阵
-			modelMat2 = glm::translate(modelMat2, cubePositions[i]); // 位移
+			modelMat = glm::translate(glm::mat4(1.0f), cubePositions[i]); // 位移
 
 			myShader->use(); // 使用着色器程序
 
+			glActiveTexture(GL_TEXTURE0); // 激活纹理单元
+			glBindTexture(GL_TEXTURE_2D, TexBufferA); // 绑定纹理
+			glActiveTexture(GL_TEXTURE3); // 激活纹理单元
+			glBindTexture(GL_TEXTURE_2D, TexBufferB); // 绑定纹理
+
 			glUniform1i(glGetUniformLocation(myShader->id, "ourTexture"), 0); // 手动设置纹理单元
 			glUniform1i(glGetUniformLocation(myShader->id, "ourFace"), 3); // 手动设置纹理单元
-			// glUniformMatrix4fv(glGetUniformLocation(myShader->id, "transform"), 1, GL_FALSE, glm::value_ptr(trans)); // 手动设置纹理单元
-			glUniformMatrix4fv(glGetUniformLocation(myShader->id, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat2)); // 手动设置纹理单元
+			glUniformMatrix4fv(glGetUniformLocation(myShader->id, "modelMat"), 1, GL_FALSE, glm::value_ptr(modelMat)); // 手动设置纹理单元
 			glUniformMatrix4fv(glGetUniformLocation(myShader->id, "viewMat"), 1, GL_FALSE, glm::value_ptr(viewMat)); // 手动设置纹理单元
 			glUniformMatrix4fv(glGetUniformLocation(myShader->id, "projectionMat"), 1, GL_FALSE, glm::value_ptr(projectionMat)); // 手动设置纹理单元
 
-			// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 绘制三角形, 6个顶点, 索引类型, 偏移量
+			glBindVertexArray(VAO); // 绑定VAO
+
 			glDrawArrays(GL_TRIANGLES, 0, 36); // 绘制立方体, 36个顶点
 		}
 
@@ -299,6 +269,7 @@ int main()
 		glfwPollEvents();
 		camera.UpdateCameraPosition();
 	}
+#pragma endregion
 
 	// 释放GLFW分配的内存
 	glfwTerminate();
