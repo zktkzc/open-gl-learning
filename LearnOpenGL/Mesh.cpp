ce3732cb39946dbe25796df90d1bdce2dfb6f9bb
tkzc00 <2238983030@qq.com>
2023-06-07 13:23:00 +0800

    Mesh（3）

--- LearnOpenGL/Mesh.cpp
+++ LearnOpenGL/Mesh.cpp
@@ -7,8 +7,29 @@ Mesh::Mesh(std::vector<Vertex> _vertices, std::vector<unsigned int> _indices, st
 	this->textures = _textures;
 }
 
+// 绘制贴图
 void Mesh::Draw(Shader* _shader)
 {
+	for (unsigned int i = 0; i < textures.size(); i++)
+	{
+		if (textures[i].type == "texture_diffuse")
+		{
+			glActiveTexture(GL_TEXTURE0); // 激活纹理单元0
+			glBindTexture(GL_TEXTURE_2D, textures[i].id); // 绑定纹理
+			_shader->SetUniform1i("material.diffuse", 0); // 设置uniform变量
+		}
+		else if (textures[i].type == "texture_specular")
+		{
+			glActiveTexture(GL_TEXTURE1); // 激活纹理单元1
+			glBindTexture(GL_TEXTURE_2D, textures[i].id); // 绑定纹理
+			_shader->SetUniform1i("material.specular", 1); // 设置uniform变量
+		}
+
+		glBindVertexArray(VAO); // 绑定顶点数组对象
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0); // 绘制网格
+		glBindVertexArray(0); // 解绑顶点数组对象
+		glActiveTexture(GL_TEXTURE0); // 激活纹理单元0
+	}
 }
 
 // 初始化网格
